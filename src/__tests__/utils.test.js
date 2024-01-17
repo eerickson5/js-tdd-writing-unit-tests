3caac0147e90ad21fe6c0f15e5613eae7cdc71ff
Elise Erickson <elisece717@gmail.com>
2024-01-17 17:44:30 -0500

    creates and satisfies tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
 // Your tests here
+import { pointsForWord } from "../utils"
+
+describe("pointsForWord", () => {
+    test("calculates the total points for a word (1 point per vowel, 2 per consonant)", () => {
+        const word = "test"
+        const points = pointsForWord(word)
+        expect(points).toBe(7)
+    })
+
+    test("handles uppercase input as well", () => {
+        const word = "GdtweGwer"
+        const points = pointsForWord(word)
+        expect(points).toBe(16)
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,5 @@
 // Your code here
+export function pointsForWord(word){
+    const vowels = ["a", "e", "i", "o", "u"]
+    return word.split('').reduce( (acc, letter) => vowels.includes(letter.toLowerCase()) ? acc + 1 : acc + 2, 0)
+}
